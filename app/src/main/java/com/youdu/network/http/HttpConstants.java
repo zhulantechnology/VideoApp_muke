8caa06dd568f19f1d9fbc873eb584473917cbc42
Jeffreywangjun <wangjunahasd@163.com>
2018-08-11 22:19:16 +0800

    换成本地Tomcat地址
    Signed-off-by: Jeffreywangjun <wangjunahasd@163.com>

--- app/src/main/java/com/youdu/network/http/HttpConstants.java
+++ app/src/main/java/com/youdu/network/http/HttpConstants.java
@@ -6,44 +6,43 @@ package com.youdu.network.http;
  * @date: 16/8/12
  */
 public class HttpConstants {
+    private static final String ROOT_URL = "http://10.0.2.2:8080";
+    public static String HOME_RECOMMAND = ROOT_URL + "/MyVideoAppServer/Data/home_data.json";
+    public static String PRODUCT_LIST = ROOT_URL + "/MyVideoAppServer/Data/search.php";
+    public static String PRODUCT_LATESAT_UPDATE = ROOT_URL + "/MyVideoAppServer/Data/upsearch.php";
+    public static String LOGIN = ROOT_URL + "/MyVideoAppServer/Data/login_phone.php";
+    public static String CHECK_UPDATE = ROOT_URL + "/MyVideoAppServer/Data/check_update.php";
+    public static String COURSE_DETAIL = ROOT_URL + "/MyVideoAppServer/Data/course_detail.php";
 
-    private static final String ROOT_URL = "http://imooc.com/api";
-   // public static String HOME_RECOMMAND = "http://10.0.2.2:8080/MyWEB13/home_data.json";
-
-    /**
-     * 请求本地产品列表
-     */
-   // public static String PRODUCT_LIST = ROOT_URL + "/fund/search.php";
-    public static String PRODUCT_LIST = "http://10.0.2.2:8080/MyWEB13/search.json";
-
-    /**
-     * 本地产品列表更新时间措请求
-     */
-    public static String PRODUCT_LATESAT_UPDATE = ROOT_URL + "/fund/upsearch.php";
-   // public static String HOME_RECOMMAND = "http://10.0.2.2:8080/MyWEB13/home_data.json";
-
-    /**
-     * 登陆接口
-     */
-    public static String LOGIN = ROOT_URL + "/user/login_phone.php";
-    //public static String HOME_RECOMMAND = "http://10.0.2.2:8080/MyWEB13/home_data.json";
-
-    /**
-     * 检查更新接口
-     */
-   // public static String CHECK_UPDATE = ROOT_URL + "/config/check_update.php";
-    public static String CHECK_UPDATE = "http://10.0.2.2:8080/MyWEB13/update.json";
-
-    /**
-     * 首页产品请求接口
-     */
-   // public static String HOME_RECOMMAND = ROOT_URL + "/product/home_recommand.php";
-    public static String HOME_RECOMMAND = "http://10.0.2.2:8080/MyWEB13/home_data.json";
-    /**
-     * 课程详情接口
-     */
-   // public static String COURSE_DETAIL = ROOT_URL + "/product/course_detail.php";
-    public static String COURSE_DETAIL = "http://10.0.2.2:8080/MyWEB13/course_detail.json";
+//    private static final String ROOT_URL = "http://imooc.com/api";
+//    /**
+//     * 请求本地产品列表
+//     */
+//   public static String PRODUCT_LIST = ROOT_URL + "/fund/search.php";
+//
+//    /**
+//     * 本地产品列表更新时间措请求
+//     */
+//    public static String PRODUCT_LATESAT_UPDATE = ROOT_URL + "/fund/upsearch.php";
+//
+//    /**
+//     * 登陆接口
+//     */
+//    public static String LOGIN = ROOT_URL + "/user/login_phone.php";
+//
+//    /**
+//     * 检查更新接口
+//     */
+//    public static String CHECK_UPDATE = ROOT_URL + "/config/check_update.php";
+//
+//    /**
+//     * 首页产品请求接口
+//     */
+//    public static String HOME_RECOMMAND = ROOT_URL + "/product/home_recommand.php";
+//    /**
+//     * 课程详情接口
+//     */
+//    public static String COURSE_DETAIL = ROOT_URL + "/product/course_detail.php";
 
 
 }
